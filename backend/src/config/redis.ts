36a388be79e2f09cd705505d1d8ba5f2c78de7df
Niranjan <niranjancb2035@gmail.com>
2026-03-22 21:21:49 +0530

    fixed backend deployment issues

--- backend/src/config/redis.ts
+++ backend/src/config/redis.ts
@@ -1,4 +1,15 @@
-export const redis = {
-    host: "127.0.0.1",
-    port: 6379
-}
+import Redis from "ioredis";
+
+const redis = new Redis(process.env.REDIS_URL as string, {
+  tls: {}, // required for Upstash
+});
+
+redis.on("connect", () => {
+  console.log("✅ Redis connected");
+});
+
+redis.on("error", (err) => {
+  console.error("❌ Redis error:", err);
+});
+
+export default redis;
